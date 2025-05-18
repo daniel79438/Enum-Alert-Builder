1c52e122fc7bf52603b2675f82206aaa496a1b7d
daniel79438 <daniel79438@gmail.com>
2025-05-18 11:47:03 +0100

    Initial Commit

---
+++ Enum Alert Builder/Enum_Alert_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enum_Alert_BuilderApp.swift
+//  Enum Alert Builder
+//
+//  Created by Daniel Harris on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Enum_Alert_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
